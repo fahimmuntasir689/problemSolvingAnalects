af40fa547776a3e93db78a9b187f73234d222e77
fahimmuntasir669 <muntasirmozumder689@gmail.com>
2025-08-18 23:15:36 +0600

    edited

--- part-05/arrayADV.js
+++ part-05/arrayADV.js
@@ -159,7 +159,17 @@ let nums = [0, 0, 1, 2, 2, 3, 4, 5, 5]
 
 let j = 1;
 
+const removeDuplicates = () => {
+    for (let i = 0; i < nums.length - 1; i++) {
+        if (nums[i] != nums[i + 1]) {
+            nums[j] = nums[i + 1]
+            j++;
+        }
 
+
+    }
+    return j
+}
 
 console.log(removeDuplicates())
 
